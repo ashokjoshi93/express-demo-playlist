bb75f5772a39ebe719282f727387a8353d904ce3
Ashok Joshi <joshiashok48684868@gmail.com>
2017-04-25 16:21:14 +0530

    - changes

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,21 +1,38 @@
-var data=[{item:'learn node'},{item:'work out'},{item:'quora'},{item:'read novel'}];
-
 var bodyParser=require('body-parser');
+var mongoose=require('mongoose');
+mongoose.connect('mongodb://admin:admin@ds117251.mlab.com:17251/tododo');
+
+var todoSchema=new mongoose.Schema({
+  item:String
+});
+
+var Todo=mongoose.model('Todo',todoSchema);
+
+
+
+//var data=[{item:'learn node'},{item:'work out'},{item:'quora'},{item:'read novel'}];
 var urlencodedParser=bodyParser.urlencoded({extended:false});
+
 module.exports=function(app){
 
   app.get('/todo',function(req,res){
-  res.render('todo',{todos:data});
+    Todo.find({},function(err,data){
+      if(err) throw wrr;
+      res.render('todo',{todos:data});
+    });
   });
 
   app.post('/todo',urlencodedParser,function(req,res){
-    data.push(req.body);
-    res.JSON(data);
+  var newTodo=Todo(req.body).save(function(err,data){
+    if(err) throw err;
+    res.json(data);
+  });
   });
 
   app.delete('/todo/:item',function(req,res){
-    data=data.filter(function(todo){
-      return todo.item.replace(/ /g,'-') !==req.params.item;
-    })
+    Todo.find({item:req.params.item.trim().replace(/\-/g," ")}).remove(function(err,data){
+      if(err) throw err;
+      res.json(data);
+    });
   });
-};
+}
